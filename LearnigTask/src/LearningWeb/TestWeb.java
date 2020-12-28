577893e31aff986d5c3851509d4b62c0d284b6d4
Nazia Yasmin <yasminnazia06@gmail.com>
2020-12-27 23:18:34 -0500

    Class creation

---
+++ LearnigTask/src/LearningWeb/TestWeb.java
@@ -0,0 +1,16 @@
+package LearningWeb;
+
+public class TestWeb {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a= 10;
+		int b=20;
+		int c=(a+b);
+		
+		System.out.println("The addition of a and b is:"+c);
+
+	}
+
+}
